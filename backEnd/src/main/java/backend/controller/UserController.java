400f8b3bff400f66e5b73ab612aa080bcfd0156a
ljh2019211415 <99960855+ljh2019211415@users.noreply.github.com>
2022-12-29 00:58:49 +0800

    结果封装

--- backEnd/src/main/java/backend/controller/UserController.java
+++ backEnd/src/main/java/backend/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
-    
+
     @GetMapping("/{id}")
     public Object test(@PathVariable("id") Long id) {
         System.out.println(id);
