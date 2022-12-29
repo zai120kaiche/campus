414e05b7dad9942c30b41deda1f1fc1d113a5129
ljh2019211415 <99960855+ljh2019211415@users.noreply.github.com>
2022-12-30 00:43:28 +0800

    init

---
+++ backend/src/test/java/com/campus/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
