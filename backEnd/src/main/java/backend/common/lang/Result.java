6960eff12bfd7f796a66ceb71dbc61464c667343
ljh2019211415 <99960855+ljh2019211415@users.noreply.github.com>
2022-12-29 23:18:05 +0800

    exception

--- backEnd/src/main/java/backend/common/lang/Result.java
+++ backEnd/src/main/java/backend/common/lang/Result.java
@@ -6,41 +6,34 @@ import java.io.Serializable;
 
 @Data
 public class Result implements Serializable {
-    private String code;
-    private String msg;
-    private Object data;
-
+    private int code;//200-correct,!200-error
+    private  String msg;
+    private  Object data;
     public static Result succ(Object data) {
-        Result m = new Result();
-        m.setCode("0");
-        m.setData(data);
-        m.setMsg("操作成功");
-        return m;
-    }
 
-    public static Result succ(String mess, Object data) {
-        Result m = new Result();
-        m.setCode("0");
-        m.setData(data);
-        m.setMsg(mess);
-        return m;
+        return succ(200, "操作成功", data);
     }
-
-    public static Result fail(String mess) {
-        Result m = new Result();
-        m.setCode("-1");
-        m.setData(null);
-        m.setMsg(mess);
-        return m;
+    public static Result succ(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
     }
-
-    public static Result fail(String mess, Object data) {
-        Result m = new Result();
-        m.setCode("-1");
-        m.setData(data);
-        m.setMsg(mess);
-        return m;
+    public static Result fail(String msg) {
+        return fail(400, msg, null);
+    }
+    public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+    public static Result fail(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
     }
 }
+
 
 
