c3ccf0c485299ffcd3d5b06f70a90a350f38906d
ljh2019211415 <99960855+ljh2019211415@users.noreply.github.com>
2022-12-30 01:40:03 +0800

    登陆测试

--- backend/src/main/java/com/campus/backend/controller/UserController.java
+++ backend/src/main/java/com/campus/backend/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.campus.backend.controller;
 
 
+import com.campus.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("/{id}")
+    public Object test(@PathVariable("id") Long id) {
+        return userService.getById(id);
+    }
 
 }
---
+++ backend/src/main/java/com/campus/backend/util/ShiroUtil.java
@@ -0,0 +1,12 @@
+package com.campus.backend.util;
+
+import com.campus.backend.shiro.AccountProfile;
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+
+    }
+
+}
