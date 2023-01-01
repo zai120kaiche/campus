1eee26979b57b638ac9e232c36441de4935985a6
Dingkai Guo <1085903802@qq.com>
2023-01-01 23:34:57 +0800

    登录注册页面part1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入新字体
+import './assets/font/font.css'
+//引入ElementUI-plus(更适用Vue3）
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 createApp(App).use(store).use(router).mount('#app')
+App.use(ElementPlus)
